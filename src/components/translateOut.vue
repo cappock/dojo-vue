f1de3dd1841d018abfa04ecf17b7a4c607dc6f43
cappock <hector.guerra@udea.edu.co>
2019-07-13 12:09:10 -0500

    first commit

---
+++ src/components/translateOut.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="translateOut">
+        <h2>{{traduciresteTexto}}</h2>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'translateOut',
+    props: ['traduciresteTexto']
+}
+</script>
+
+<style>
+
+</style>
